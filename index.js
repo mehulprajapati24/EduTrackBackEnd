8f072f6e710969b212730050fca86298fe2efcd4
Mehul Prajapati <mehulkumarprajapati21@gnu.ac.in>
2024-12-07 00:52:32 +0530

    backend part completed

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-    origin: 'https://edu-track-uvpce.vercel.app', // Your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-};
-
-// Use CORS middleware with options
-app.use(cors(corsOptions));
-// app.use(cors());
+app.use(cors());
 
 async function main() {
     await mongoose.connect(process.env.CONNECTION_STRING, {
